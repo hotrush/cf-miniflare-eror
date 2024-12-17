2fc2f071b4ba087c527493d120f558c1188035a1
hotrush <johny64@gmail.com>
2024-12-17 17:00:17 +0100

    ...

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,20 @@
 // test/index.spec.ts
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
+import { Miniflare } from 'miniflare';
 import worker from '../src/index';
+// import * as fs from 'fs';
+// import path from 'path';
+
+const mf = new Miniflare({
+	modules: true,
+	scriptPath: '../src/index.ts',
+	r2Buckets: ['R2_BUCKET'],
+});
+
+// This code doesn't affect since error happens before
+// const b = await mf.getR2Bucket('R2_BUCKET');
+// await b.put('source-image.png', fs.readFileSync(path.resolve(__dirname, './test-image.jpg')))
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
